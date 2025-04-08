fc24c29a72b449d09885d223f65c34b2cea4ca2c
Morrowindchamp <30811747+Morrowindchamp@users.noreply.github.com>
2025-04-07 22:04:55 -0500

    Update SafeArray.py
    Negative array indexing

--- SafeArray.py
+++ SafeArray.py
@@ -5,12 +5,18 @@ class SafeArray(list):
 
     def __getitem__(self, index):
         if isinstance(index, int):  # Handle single index access
-            if 0 <= index < len(self):
+            if -len(self) <= index < len(self):  # Adjust for negative indices
                 return list.__getitem__(self, index)
             return self.placeholder
         elif isinstance(index, slice):  # Handle slice access
-            # Constrain stop value to len(self)
-            start, stop, step = index.start or 0, min(index.stop or len(self), len(self)), index.step or 1
-            return SafeArray([list.__getitem__(self, i) for i in range(start, stop, step) if 0 <= i < len(self)], placeholder=self.placeholder)
+            start, stop, step = (
+                index.start or 0,
+                min(index.stop or len(self), len(self)),
+                index.step or 1,
+            )
+            return SafeArray(
+                [list.__getitem__(self, i) for i in range(start, stop, step) if -len(self) <= i < len(self)],
+                placeholder=self.placeholder,
+            )
         else:
             raise TypeError("Invalid index type")
